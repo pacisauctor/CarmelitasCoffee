d20537989f707965b98ae928d698d05ac832ec22
Axel Eleazar García Darce <darce@localhost>
2019-08-02 13:01:07 -0600

    agregando hibernate

--- CarmelitasCoffee/src/com/carmelitascoffee/controlador/Principal.java
+++ CarmelitasCoffee/src/com/carmelitascoffee/controlador/Principal.java
@@ -6,19 +6,26 @@
 package com.carmelitascoffee.controlador;
 
 import com.carmelitascoffee.vista.inicio.InicioSesion;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
  * @author darce
  */
 public class Principal {
-        public static void main(String[] args) {
+
+    public static void main(String[] args) {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> {
+            
             InicioSesion inicioSesion = new InicioSesion("Inicio de Sesión");
             inicioSesion.setSize(280, 350);
             inicioSesion.setVisible(true);
             inicioSesion.setLocationRelativeTo(null);
+
         });
     }
 }
