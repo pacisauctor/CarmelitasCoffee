4a1b917bc8f71e0e1a0f110895d8693dcfd52d2c
Axel García <agarciadarce@gmail.com>
2019-12-03 11:38:20 -0600

    agregando sql

---
+++ sql/crearUsuario_Usuario-root.sql
@@ -0,0 +1,5 @@
+CREATE USER 'coffee'@'localhost' IDENTIFIED BY 'CarmelitasCoffee100!';
+GRANT SELECT ON performance_schema.* TO 'coffee'@'localhost';
+CREATE SCHEMA carmelitas_coffee;
+GRANT ALL PRIVILEGES ON carmelitas_coffee.* TO 'coffee'@'localhost';
+FLUSH privileges;
