dafd07b2ae05184f7379e08b0a92e4ec307f38e4
Axel García <agarciadarce@gmail.com>
2019-12-22 18:45:18 -0600

    frame de Cargando

--- CarmelitasCoffee/src/com/carmelitascoffee/controlador/Principal.java
+++ CarmelitasCoffee/src/com/carmelitascoffee/controlador/Principal.java
@@ -6,6 +6,9 @@
 package com.carmelitascoffee.controlador;
 
 import com.carmelitascoffee.vista.inicio.InicioSesion;
+import com.carmelitascoffee.vista.inicio.LoadingApplication;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.hibernate.Session;
 
 /**
@@ -16,11 +19,43 @@ public class Principal {
 
     public static void main(String[] args) {
         /* Create and display the form */
+        CargandoApp app = new CargandoApp();
+        app.start();
         Session s = HibernateUtil.getSessionFactory().openSession();
+        app.terminar();
         InicioSesion inicioSesion = new InicioSesion("Inicio de Sesión", s);
         inicioSesion.setSize(280, 350);
         inicioSesion.setVisible(true);
         inicioSesion.setLocationRelativeTo(null);
 
     }
+}
+
+class CargandoApp extends Thread {
+
+    boolean cargando = true;
+    LoadingApplication primero;
+
+    public CargandoApp() {
+        primero = new LoadingApplication();
+    }
+
+    public void terminar() {
+        cargando = false;
+        primero.setVisible(false);
+    }
+
+    @Override
+    public void run() {
+        while (cargando) {
+            try {
+                Thread.sleep(500);
+                System.out.println("repetir");
+                primero.cambiarValorAumentandoEn(10);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(CargandoApp.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
 }
