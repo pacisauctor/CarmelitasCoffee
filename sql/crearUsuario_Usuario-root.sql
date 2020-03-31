d06f43a2b5516b8cee67f0c693e95ba4b877570f
Axel Eleazar Garcia Darce <agarciadarce@gmail.com>
2020-03-30 21:01:04 -0600

    i dont know

--- sql/crearUsuario_Usuario-root.sql
+++ sql/crearUsuario_Usuario-root.sql
@@ -3,3 +3,5 @@ GRANT SELECT ON performance_schema.* TO 'coffee'@'localhost';
 CREATE SCHEMA carmelitas_coffee;
 GRANT ALL PRIVILEGES ON carmelitas_coffee.* TO 'coffee'@'localhost';
 FLUSH privileges;
+
+SELECT @@global.time_zone, @@session.time_zone;
